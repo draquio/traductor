a18aace6310937637f50d34002a51761a8d561af
draquio <draquio@gmail.com>
2023-12-07 18:00:22 -0400

    quitando mi api personal

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const ENV = {
-    Api_key: "3c80c54693mshb638f172ee3d6e4p18ffe3jsnd80c1632df47",
+    Api_key: "Tu-api-key",
     Api_url: "https://microsoft-translator-text.p.rapidapi.com/translate",
-    Api_host: "microsoft-translator-text.p.rapidapi.com"
+    Api_host: ""
 }
 
 export default ENV;
