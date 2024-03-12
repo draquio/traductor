7582c9ccdf2089bc8da018fce0319993c273c689
draquio <draquio@gmail.com>
2024-03-12 15:18:27 -0400

    cambiando logo

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,7 +3,7 @@ const Nav = () => {
   return (
     <div className="nav_content">
       <nav className="nav_class">
-        <img src="/logo.png" alt="Logo Traductor" className="logo" />
+        <img src="/logo.webp" alt="Logo Traductor" className="logo" />
       </nav>
     </div>
   )
